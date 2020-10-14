3b75b93383924e7f3b785901273ef466a12e42f7
Prateek <prateeksethi2106@gmail.com>
2020-10-14 13:04:26 +0530

    initial commit

---
+++ src/test/java/functionalTests/GetRequest.java
@@ -0,0 +1,31 @@
+package functionalTests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.RestAssured.*;
+import io.restassured.matcher.RestAssuredMatchers.*;
+import io.restassured.response.Response;
+import testBase.TestBase;
+
+public class GetRequest extends TestBase{
+	@Test
+	public void getRequest() {
+		
+		RestAssured.baseURI="https://reqres.in/api/users";
+
+		
+		Response response=RestAssured.given().param("page", "2")
+		.when()
+		.get();
+		System.out.println(response.getBody().asString());
+		System.out.println("get Status code "+response.getStatusCode());
+		System.out.println("get ContentType "+response.getContentType());
+		System.out.println("get Time "+response.getTime());
+		System.out.println("get Cokies "+response.getCookies());
+		System.out.println("get pretty response "+response.prettyPrint());
+		
+	}
+
+}
